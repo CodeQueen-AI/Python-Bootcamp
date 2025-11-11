b5f2505361a8e0bc835ff83610db3b5e666dde9a
Sumbal Naz <Codeq209@gmail.com>
2025-11-11 20:22:54 +0500

    Add files via upload

---
+++ 13-PEDMAS/index.py
@@ -0,0 +1,31 @@
+# Simple Addition & Multiplication
+result = 2 + 3 * 4
+print(result)  
+
+result = (2 + 3) * 4
+print(result)  
+
+# Subtraction & Division
+result = 20 - 4 / 2
+print(result)  
+
+result = (20 - 4) / 2
+print(result)  
+
+# Exponents + Addition/Subtraction
+result = 2 + 3 ** 2 - 1
+print(result)  
+
+result = (2 + 3) ** 2 - 1
+print(result)  
+
+# Complex Expression
+a, b, c, d, e = 1, 2, 3, 5, 8
+
+# Without parentheses
+final = a + b * c - d / e
+print(final)
+
+# With parentheses (clear)
+final = (a + b) * (c - (d / e))
+print(final)
