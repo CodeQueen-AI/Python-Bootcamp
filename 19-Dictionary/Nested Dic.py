cd5f2be16bb2a3538c3b738dc3093f815bdc21e1
Sumbal Naz <Codeq209@gmail.com>
2025-11-19 14:41:58 +0500

    Update Nested Dic.py

--- 19-Dictionary/Nested Dic.py
+++ 19-Dictionary/Nested Dic.py
@@ -1,26 +1,12 @@
-students = {
-    "student1": {
-        "name": "Ali",
-        "age": 20,
-        "course": "Python"
-    },
-    "student2": {
-        "name": "Sara",
-        "age": 22,
-        "course": "Java"
+student = {
+    "name": "Code Queen",
+    "age": 21,
+    "address": {
+        "city": "Lahore",
+        "area": "Model Town",
+        "zip": 54000
     }
 }
 
-# Accessing elements
-print(students["student1"]["name"])   
-print(students["student2"]["course"]) 
-
-# Modifying nested dictionary
-students["student1"]["age"] = 21
-print(students["student1"]["age"])   
-
-# Iterating through nested dictionary
-for student, info in students.items():
-    print(student, "->")
-    for key, value in info.items():
-        print("   ", key, ":", value)
+print(student["name"])       
+print(student["address"]["city"]) 
