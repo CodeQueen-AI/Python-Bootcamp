bd84084038ea50082d09fe81150383b4b4df3689
Sumbal Naz <Codeq209@gmail.com>
2025-11-19 15:49:23 +0500

    Update String Formatting.py

--- 15-String/String Formatting.py
+++ 15-String/String Formatting.py
@@ -1,1 +1,28 @@
+# f-string
+name = 'CodeQueen'
+age = 18
+print(f'My name is {name} and I am {age} Years old.')
+
+a = 10
+b = 15
+print(f'The sum of {a} & b is {a + b}')
+
+
+
+
+# Format() Methods
+name = 'CodeQueen'
+age = 18
+print('My name is {} and I am {} Years Old' .format(name, age))
+
+# Using Index Numbers
+print('My name is {0} and I am {1} Years Old.' .format(name, age))
+
+# Using PlaceHolders
+print('My Name is {n} and I am {a} Years Old.' .format(n=name, a=age))
+
+# Expressions
+a = 10
+b = 3
+print('The sum of {} and {} is {}' .format(a, b , a+b))
 
