7df0e2f5b0cb1c5b108fca81aa843e2ae19ee9f3
CodeQueen-AI <codeq209@gmail.com>
2026-01-06 01:11:02 +0500

    update

--- 10-Conditional Statements/index.py
+++ 10-Conditional Statements/index.py
@@ -1,17 +1,18 @@
-# If Statements
+# Conditional Stataments : Decides which code block runs based on conditions.
+
+# If Statements : Checks only one condition
 age = 18
 if age >= 18:
     print("You are eligible to vote!")
     
-# If-Else
+# If-Else : Runs one block if a condition is true, otherwise runs another block
 age = 16
 if age >= 18:
     print("You are eligible to vote!")
 else:
     print("You are NOT eligible to vote")
 
-
-# If-elif-else
+# If-elif-else : Checks multiple conditions; runs the first true block or else block
 marks = 85
 if marks >= 90:
     print("Grade: A")
@@ -22,8 +23,7 @@ elif marks >= 70:
 else:
     print("Grade: F")
 
-
-# Nested If
+# Nested If : An if statement inside another if statement.
 age = 20
 
 if age >= 18:
