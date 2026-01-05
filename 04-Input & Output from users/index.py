bc7256131d8751b84248001b558129bd1e9e72b0
CodeQueen-AI <codeq209@gmail.com>
2026-01-06 00:53:42 +0500

    update

--- 04-Input & Output from users/index.py
+++ 04-Input & Output from users/index.py
@@ -2,10 +2,6 @@
 name = input("Enter your name: ")
 print("Hello, " + name + "!")  
 
-#Type casting with input
-age = int(input("Enter your age: "))  
-print("Your age is:", age)
-
 #Output 
 name = "Code Queen"
 print("Hello,", name)  
