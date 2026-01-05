a1f80845247ac8e32f2cd4b58a6dad891df63f99
CodeQueen-AI <codeq209@gmail.com>
2026-01-06 01:13:26 +0500

    update

--- 09-Operators/Arithematic Operators.py
+++ 09-Operators/Arithematic Operators.py
@@ -1,3 +1,7 @@
+# Operator: A symbol that performs an action
+# Operation: The result of an operator acting on numbers
+# Arithmetic Operators: Symbols used to perform calculations on numbers
+
 print(10 + 5)
 print(3 - 1)
 print(4 * 2)
