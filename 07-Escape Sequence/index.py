255f8d08057f660758de294bb4d2994785f439e8
CodeQueen-AI <codeq209@gmail.com>
2026-01-06 01:02:10 +0500

    update

---
+++ 07-Escape Sequence/index.py
@@ -0,0 +1,26 @@
+# Escape Sequence : Escape sequences are special codes in Python that start with a backslash ()
+# and are used to control or format text output in print statements
+
+#\n : New line   
+print("Hello\nWorld")
+
+#\t : Tab space  
+print("Hello\tWorld")
+
+#\\ : Backslash
+print("This is a backslash: \\")
+
+#\' : Single quote
+print('It\'s a beautiful day!')
+
+#\" : Double quote
+print("She said, \"Python is fun!\"")
+
+#\r : Carriage return
+print("Hello\rWorld")
+
+#\b : Backspace
+print("Hello\bWorld")
+
+#\f : Form feed
+print("Hello\fWorld")
