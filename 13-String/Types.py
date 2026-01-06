862da048333242edfe6d57b8f6b14edf3bb1da99
CodeQueen-AI <codeq209@gmail.com>
2026-01-06 14:15:12 +0500

    update

---
+++ 13-String/Types.py
@@ -0,0 +1,21 @@
+# String : A string is a collection of characters (text) written inside quotes
+
+# Single Quotes : Used to define simple text
+single_quote = 'Single Quotes'
+print(single_quote)
+
+# Double Quotes : Used when the text contains single quotes
+double_quote = "Double Quotes"
+print(double_quote)
+
+# Triple Quotes : Used to define multi-line text
+multi_line = """Triple Quotes"""
+print(multi_line)
+
+# Empty String : A string with no value
+empty = ""
+print(empty)
+
+# String Length (len()) : its used to count characters in a string
+text = "Python"
+print(len(text))
