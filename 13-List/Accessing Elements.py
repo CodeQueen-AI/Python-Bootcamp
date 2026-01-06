edbd9a45bb71074f790ec6cc49d14560d11bd2bf
CodeQueen-AI <codeq209@gmail.com>
2026-01-06 14:34:43 +0500

    update

--- 13-List/Accessing Elements.py
+++ 13-List/Accessing Elements.py
@@ -1,13 +1,13 @@
 list = [10, 20, 30, 40, 50]
 
-# Indexing
+# Indexing : Access individual items by their position
 print(list[0])  
 print(list[2])  
 print(list[-1])  
 print(list[-3])  
 
-# Slicing
-print(list[1:4])  
+# Slicing : Access a range or step of items
+print(list[1:4])
 print(list[:3])  
-print(list[2:])  
+print(list[2:])   
 print(list[::2])  
--- 13-List/List conversion.py
+++ 13-List/List conversion.py
@@ -1,7 +1,9 @@
-# From string
-str_list = list("hello")
-print(str_list)  
+# List Conversion : converting other data types into a list
 
-# From tuple
+# From string : each character becomes a list item
+str_list = list("hello")
+print(str_list) 
+
+# From tuple : tuple elements become list items
 tuple_list = list((1, 2, 3))
-print(tuple_list) 
+print(tuple_list)  
